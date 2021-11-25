285b7e830ea89a2ee8d341a73564ad8c2ef28a08
damien-simplon <damien.bellec12@gmail.com>
2021-11-25 15:34:47 +0100

    création des fonctions post, put et delete pour les alertes

--- Controllers/alertController.js
+++ Controllers/alertController.js
@@ -8,5 +8,15 @@ module.exports = {
     getAlertById: async (req, res) => {
         const alert = await alertModel.findById(req.params.id);
         res.status(200).json({alert});
+    },
+    postAlert: async (req, res) => {
+        const alert = await alertModel.create(req.body);
+        res.status(201).json({alert});
+    },
+    putAlert: async (req, res) => {
+        const alert = await alertModel.findByIdAndUpdate(req.params.id, req.body);
+    },
+    deleteAlert: async (req, res) => {
+        const alert = await alertModel.findByIdAndDelete(req.params.id);
     }
 };
