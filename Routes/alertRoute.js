c6aba4ce8fb6200ec9ad92e0c98794ecece0b52f
damien-simplon <damien.bellec12@gmail.com>
2021-11-25 15:35:28 +0100

    création des routes pour les fonctions post, put et delete pour alert

--- Routes/alertRoute.js
+++ Routes/alertRoute.js
@@ -4,4 +4,12 @@ const route = express.Router();
 
 route.get('/', alertController.getAllAlert);
 
+route.get('/:id', alertController.getAlertById);
+
+route.post('/', alertController.postAlert);
+
+route.put('/:id', alertController.putAlert);
+
+route.delete('/:id', alertController.deleteAlert);
+
 module.exports = route;
