d3824f5fd57c21b27bebf8b581ecb05924625e64
damien-simplon <damien.bellec12@gmail.com>
2021-12-02 10:46:08 +0100

    création du user model avec un email et un mot de passe

---
+++ Models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "l'email est requis"],
+        unique: [true, "cette adresse email existe déjà"],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Merci de fournir un email valide',
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "le mot de passe est requis"],
+        minlength: [8, "le mot de passe doit contenir au moins 8 caractères"],
+        maxlength: [32, "le mot de passe doit contenir au plus 32 caractères"],
+    }
+},{
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
